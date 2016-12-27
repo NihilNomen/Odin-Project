1a62436971d519ef3556b37e8ac15ad04ec93108
Michael Lai <michael.laifreedom@gmail.com>
2016-12-27 17:10:41 -0500

    Create strings.rb

---
+++ Ruby/Chris Pine Learn to Program/Chapter 2/strings.rb
@@ -0,0 +1,23 @@
+#Learn to Program Chapter 2 by Chris Pine (Odin Project Ruby)
+puts 'Hello, world!'
+puts ' '
+puts 'Goodbye.'' 
+
+#Arithmetic
+puts 'I like'+ ' ' + 'apple pie.'
+
+puts 'blink ' * 4
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+#These will not work
+#puts '12' + 12
+#puts '2' * '5'
+
+puts 'You\'re swell!'
