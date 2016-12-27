abe1771a60c257c8589ccb8310fe34ce9afda111
Michael Lai <michael.laifreedom@gmail.com>
2016-12-27 17:33:13 -0500

    Create methods3.rb

---
+++ Ruby/Chris Pine Learn to Program/Chapter 8/methods3.rb
@@ -0,0 +1,12 @@
+#Chapter 8 by Chris Pine (Odin Project Ruby)
+#method parameters
+
+def sayMoo numberOfMoos
+	puts 'mooooooo...' * numberOfMoos
+end
+
+sayMoo 3
+puts 'oink-oink'
+
+#uncommenting the should give error, no arguments
+#sayMoo
