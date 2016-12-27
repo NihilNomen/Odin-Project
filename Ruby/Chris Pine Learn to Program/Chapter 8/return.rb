fddfa4f9509daf8fa98176136af2e11e0fce4147
Michael Lai <michael.laifreedom@gmail.com>
2016-12-27 17:34:53 -0500

    Create return.rb

---
+++ Ruby/Chris Pine Learn to Program/Chapter 8/return.rb
@@ -0,0 +1,11 @@
+#Chapter 8 by Chris Pine (Odin Project Ruby)
+returnVal = puts 'This puts returned: '
+puts returnVal
+
+def sayMoo numberOfMoos
+	puts 'mooooooo...'*numberOfMoos
+	'yellow submarine'
+end
+
+x = sayMoo 2
+puts x
