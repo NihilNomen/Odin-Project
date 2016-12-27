56094efe236052f6f09a280bbce0c316a47d8c29
Michael Lai <michael.laifreedom@gmail.com>
2016-12-27 17:12:26 -0500

    Create variables2.rb

---
+++ Ruby/Chris Pine Learn to Program/Chapter 4/variables2.rb
@@ -0,0 +1,23 @@
+#Learn to Program Chapter 4 by Chris Pine (Odin Project Ruby)
+var1 = 2
+var2 = '5'
+
+#can't add string and integer
+#puts var1 + var2
+
+#convert integer to string
+puts var1.to_s + var2
+puts var1 + var2.to_i
+
+puts '15'.to_f
+puts '99.999'.to_f
+puts '99.999'.to_i
+puts ''
+puts 'stringy'.to_s
+puts 3.to_i
+
+puts 20
+puts 20.to_s
+puts '20'
+
+puts gets
