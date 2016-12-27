8c7e12388b8caa352487f3c1047f7ace0bafb82d
Michael Lai <michael.laifreedom@gmail.com>
2016-12-27 17:28:52 -0500

    Create array3.rb

---
+++ Ruby/Chris Pine Learn to Program/Chapter 7/array3.rb
@@ -0,0 +1,15 @@
+#Chapter 7 by Chris Pine (Odin Project Ruby)
+foods = ['artichoke', 'brioche', 'caramel']
+
+puts foods
+puts
+puts foods.to_s
+puts
+puts foods.join(', ')
+puts
+puts foods.join(' :) ') + ' 8)'
+
+#30.times do doesn't change a thing!!!!!!!!!!!
+30.times do
+	puts []
+end
