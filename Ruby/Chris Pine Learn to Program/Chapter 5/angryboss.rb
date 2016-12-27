5c485e82d40d4b9a092218b0464b4c330c97cec3
Michael Lai <michael.laifreedom@gmail.com>
2016-12-27 17:15:35 -0500

    Create angryboss.rb

---
+++ Ruby/Chris Pine Learn to Program/Chapter 5/angryboss.rb
@@ -0,0 +1,12 @@
+#Chapter 5 by Chris Pine (Odin Project Ruby)
+puts 'Boss: What do you want?'
+want = gets.chomp
+puts 'I want a ' + want
+
+if want = "raise"
+	puts 'Boss: You want a WHAT?'
+	puts 'I want a ' + want
+	puts "WHADDAYA mean 'I want a raise' You're fired! Get out off my office!"
+else
+	puts ('Ok sure.')
+end
