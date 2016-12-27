6e657cb11c02ac3ba68668a14f32c4ae8efa6efe
Michael Lai <michael.laifreedom@gmail.com>
2016-12-27 17:13:43 -0500

    Create ch4end.rb

---
+++ Ruby/Chris Pine Learn to Program/Chapter 4/ch4end.rb
@@ -0,0 +1,12 @@
+#Learn to Program Chapter 4 by Chris Pine (Odin Project Ruby)
+puts 'What is your first name?'
+firstname = gets.chomp
+
+puts 'What is your middle name?'
+middlename = gets.chomp
+
+puts 'What about your last name or surname' + '?'
+surname = gets.chomp
+
+puts 'Ok'
+puts 'Hello there, ' + firstname + ' ' + middlename + ' ' + surname + '!'
