d2ffaf1c65e586abf2c67ff391c07b86ca2788c4
Michael Lai <michael.laifreedom@gmail.com>
2016-12-27 17:16:29 -0500

    Create math2.rb

---
+++ Ruby/Chris Pine Learn to Program/Chapter 5/math2.rb
@@ -0,0 +1,16 @@
+#Chapter 5 by Chris Pine (Odin Project Ruby)
+#srand = seed random
+
+srand 1776
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts ''
+srand 1776
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
