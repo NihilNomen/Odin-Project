b18fe21f44afe844e481f83100ee69227f0be062
Michael Lai <michael.laifreedom@gmail.com>
2016-12-27 17:19:35 -0500

    Create flowcontrol3.rb

---
+++ Ruby/Chris Pine Learn to Program/Chapter 6/flowcontrol3.rb
@@ -0,0 +1,11 @@
+#Chapter 6 by Chris Pine (Odin Project Ruby)
+puts 'Type Something!'
+
+command = ' '
+
+while command != 'bye'
+	puts command
+	command = gets.chomp
+end
+
+puts 'Come again soon!'
